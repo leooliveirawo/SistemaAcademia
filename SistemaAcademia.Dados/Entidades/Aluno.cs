6fb5172f31e403ca8a9db7a3bb8cf85c62fe62db
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2022-11-22 18:08:30 -0300

    Criou a classe dos alunos.

---
+++ SistemaAcademia.Dados/Entidades/Aluno.cs
@@ -0,0 +1,18 @@
+﻿namespace SistemaAcademia.Dados.Entidades
+{
+    public class Aluno : Entidade
+    {
+        public string NomeCompleto { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco { get; set; }
+        public string Complemento { get; set; }
+        public string Referencia { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string EstadoUf { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+        public string Telefone3 { get; set; }
+        public string Email { get; set; }
+    }
+}
