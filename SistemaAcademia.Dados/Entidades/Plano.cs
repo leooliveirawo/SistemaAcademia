62da0e83e62aada4510eccf1cc04b5c793398ac9
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2022-11-22 18:11:01 -0300

    Criou a classes dos planos.

---
+++ SistemaAcademia.Dados/Entidades/Plano.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaAcademia.Dados.Entidades
+{
+    public class Plano
+    {
+        public string Nome { get; set; }
+        public int DiasValidos { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroParcelas { get; set; }
+    }
+}
