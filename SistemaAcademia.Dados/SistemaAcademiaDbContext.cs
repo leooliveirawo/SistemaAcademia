63c8f210898ea5b82970b14b4bcadaf37d446006
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2022-11-22 18:37:58 -0300

    Criou classe do DbContext do EF.

---
+++ SistemaAcademia.Dados/SistemaAcademiaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaAcademia.Dados.Entidades;
+
+namespace SistemaAcademia.Dados
+{
+    public class SistemaAcademiaDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Plano> Planos { get; set; }
+        public DbSet<Exercicio> Exercicios { get; set; }
+        public DbSet<TreinoExercicio> TreinosExercicios { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+        public DbSet<Treino> Treinos { get; set; }
+        public DbSet<TreinoExercicio> TreinosExercicios { get; set; }
+    }
+}
